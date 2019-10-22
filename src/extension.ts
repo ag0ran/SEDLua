deab8c92af03b41ae1c1ee6b2eced36992e03ccb
unknown <agoran@gmail.com>
2019-10-22 20:34:04 +0200

    AST is used to find member expression. AST resulting from last parse is maintaned (ranges are fixed if there is currently an error).

--- src/extension.ts
+++ src/extension.ts
@@ -103,7 +103,7 @@ class SEDLua implements vscode.CompletionItemProvider {
     if (iTokenAtOffset === -1) {
       return undefined;
     }
-    let lastInfo = completionInfo.resolveIndexingExpressionAtToken(iTokenAtOffset, this.helpCompletionInfo);
+    let lastInfo = completionInfo.resolveMemberExpressionAtOffset(offset, this.helpCompletionInfo);
 
     let hover: vscode.Hover|undefined;
     if (lastInfo instanceof MacroClassCompletionInfo) {
@@ -202,7 +202,7 @@ class SEDLua implements vscode.CompletionItemProvider {
     if (!documentCompletionHandler) {
       return;
     }
-    documentCompletionHandler.onDocumentChanged(e.document);
+    documentCompletionHandler.onDocumentChanged(e);
     let documentCompletionInfo = await documentCompletionHandler.getCompletionInfoNow();
     if (!documentCompletionInfo) {
       return;
