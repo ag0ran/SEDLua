2815b3dbee732a114f41c335a804beee1a2e3f2e
unknown <agoran@gmail.com>
2019-11-10 13:54:12 +0100

    WIP: typescript lua lexer and parser

---
+++ src/luaLexer.ts
@@ -0,0 +1,559 @@
+export enum LuaTokenType {
+  Unexpected = -1,
+  EOF = 0,
+  StringLiteral,
+  Keyword,
+  Identifier,
+  NumericLiteral,
+  Punctuator,
+  BooleanLiteral,
+  NilLiteral,
+  VarargLiteral,
+  Comment,
+}
+
+let errorStrings = {
+  unexpected: 'unexpected %1 \'%2\' near \'%3\'',
+  expected: '\'%1\' expected near \'%2\'',
+  expectedToken: '%1 expected near \'%2\'', 
+  unfinishedString: 'unfinished string near \'%1\'',
+  malformedNumber: 'malformed number near \'%1\'', 
+  invalidVar: 'invalid left-hand side of assignment near \'%1\'',
+};
+
+export class LuaToken {
+  constructor(type: LuaTokenType, value: string, line: number, lineStart: number, rangeStart: number, rangeEnd: number) {
+    this.type = type;
+    this.value = value;
+    this.line = line;
+    this.lineStart = lineStart;
+    this.rangeStart = rangeStart;
+    this.rangeEnd = rangeEnd;
+  }
+  type: LuaTokenType;
+  value: string;
+  line: number;
+  lineStart: number;
+  rangeStart: number;
+  rangeEnd: number;
+}
+
+export interface LuaSyntaxError {
+  message: string;
+  line: number;
+  column: number;
+  endColumn: number;
+}
+
+
+export interface LuaLexer {
+  getNextToken(): LuaToken;
+  raiseError(token: LuaToken, ...args: any[]): void;
+  reset(): void;
+  readonly errors: Array<LuaSyntaxError>;
+}
+
+export function LuaLexer(inputSource: string): LuaLexer {
+  let input: string = inputSource;
+  let index = 0;
+  let line = 1;
+  let lineStart = 0;
+  let length = input.length;
+  let errors = new Array<LuaSyntaxError>();
+
+  function raiseError(token: LuaToken|undefined, errorFormat: string, ...args: string[]) {
+    let message = sprintf(errorFormat, ...args);
+    let error = {
+      message: sprintf(errorFormat, ...args),
+      line: line,
+      column: 0,
+      endColumn: 0,
+    };
+    if (token) {
+      error.column = token.rangeStart - token.lineStart;
+      error.endColumn = token.rangeEnd - token.lineStart;
+    } else {
+      error.column = index - lineStart;
+      error.endColumn = 10000;
+    }
+    errors.push(error);
+  }
+
+  // Whitespace has no semantic meaning in lua so simply skip ahead while
+  // tracking the encounted newlines. Any kind of eol sequence is counted as a
+  // single line.
+  function consumeEOL() {
+    var charCode = input.charCodeAt(index)
+      , peekCharCode = input.charCodeAt(index + 1);
+
+    if (isLineTerminator(charCode)) {
+      // Count \n\r and \r\n as one newline.
+      if (10 === charCode && 13 === peekCharCode) {
+        index++;
+      }
+      if (13 === charCode && 10 === peekCharCode) {
+        index++;
+      }
+      line++;
+      lineStart = ++index;
+      return true;
+    }
+    return false;
+  }
+
+  function skipWhiteSpace() {
+    while (index < length) {
+      var charCode = input.charCodeAt(index);
+      if (isWhiteSpace(charCode)) {
+        index++;
+      } else if (!consumeEOL()) {
+        break;
+      }
+    }
+  }
+
+  // Read a multiline string by calculating the depth of `=` characters and
+  // then appending until an equal depth is found.
+  function readLongString(): string|undefined {
+    let level = 0;
+    let content = '';
+    let terminator = false;
+    let character;
+    let stringStart;
+
+    index++; // [
+
+    // Calculate the depth of the comment.
+    while ('=' === input.charAt(index + level)) {
+      level++;
+    }
+    // Exit, this is not a long string afterall.
+    if ('[' !== input.charAt(index + level)) {
+      return undefined;
+    }
+
+    index += level + 1;
+
+    // If the first character is a newline, ignore it and begin on next line.
+    if (isLineTerminator(input.charCodeAt(index))) {
+      consumeEOL();
+    }
+
+    stringStart = index;
+    while (index < length) {
+      // To keep track of line numbers run the `consumeEOL()` which increments
+      // its counter.
+      if (isLineTerminator(input.charCodeAt(index))) {
+        consumeEOL();
+      }
+
+      character = input.charAt(index++);
+
+      // Once the delimiter is found, iterate through the depth count and see
+      // if it matches.
+      if (']' === character) {
+        terminator = true;
+        for (var i = 0; i < level; i++) {
+          if ('=' !== input.charAt(index + i)) {
+            terminator = false;
+          }
+        }
+        if (']' !== input.charAt(index + level)) {
+          terminator = false;
+        }
+      }
+
+      // We reached the end of the multiline string. Get out now.
+      if (terminator) {
+        break;
+      }
+    }
+    content += input.slice(stringStart, index - 1);
+    index += level + 1;
+
+    return content;
+  }
+
+  // Translate escape sequences to the actual characters.
+  function readEscapeSequence() {
+    var sequenceStart = index;
+    switch (input.charAt(index)) {
+      // Lua allow the following escape sequences.
+      // We don't escape the bell sequence.
+      case 'n': index++; return '\n';
+      case 'r': index++; return '\r';
+      case 't': index++; return '\t';
+      case 'v': index++; return '\x0B';
+      case 'b': index++; return '\b';
+      case 'f': index++; return '\f';
+      // Skips the following span of white-space.
+      case 'z': index++; skipWhiteSpace(); return '';
+      // Byte representation should for now be returned as is.
+      case 'x':
+        // \xXX, where XX is a sequence of exactly two hexadecimal digits
+        if (isHexDigit(input.charCodeAt(index + 1)) &&
+          isHexDigit(input.charCodeAt(index + 2))) {
+          index += 3;
+          // Return it as is, without translating the byte.
+          return '\\' + input.slice(sequenceStart, index);
+        }
+        return '\\' + input.charAt(index++);
+      default:
+        // \ddd, where ddd is a sequence of up to three decimal digits.
+        if (isDecDigit(input.charCodeAt(index))) {
+          while (isDecDigit(input.charCodeAt(++index))) {}
+          return '\\' + input.slice(sequenceStart, index);
+        }
+        // Simply return the \ as is, it's not escaping any sequence.
+        return input.charAt(index++);
+    }
+  }
+
+  function scanComment() {
+    let tokenStart = index;
+    index += 2; // --
+
+    var character = input.charAt(index)
+      , content = ''
+      , isLong = false
+      , commentStart = index
+      , lineStartComment = lineStart
+      , lineComment = line;
+
+    if ('[' === character) {
+      let longString = readLongString();
+      // This wasn't a multiline comment after all.
+      if (!longString) {
+        content = character;
+      } else {
+        content = longString;
+        isLong = true;
+      }
+    }
+    // Scan until next line as long as it's not a multiline comment.
+    if (!isLong) {
+      while (index < length) {
+        if (isLineTerminator(input.charCodeAt(index))) {
+          break;
+        }
+        index++;
+      }
+      content = input.slice(commentStart, index);
+    }
+    return new LuaToken(LuaTokenType.Comment, content, lineComment, lineStart, tokenStart, index);    
+  }
+
+  function getNextToken(): LuaToken {
+    // Comments begin with -- after which it will be decided if they are
+    // multiline comments or not.
+    //
+    // The multiline functionality works the exact same way as with string
+    // literals so we reuse the functionality.
+    
+    skipWhiteSpace();
+
+    // Skip comments beginning with --
+    if (45 === input.charCodeAt(index) && 45 === input.charCodeAt(index + 1)) {
+      return scanComment();
+    }
+
+
+    if (index >= length) {
+      return new LuaToken(LuaTokenType.EOF, '<eof>', line, lineStart, index, index);
+    }
+
+    let tokenStart = index;
+    function scanIdentifierOrKeyword() {
+      let value;
+      let type;
+
+      while (isIdentifierPart(input.charCodeAt(++index))) {}
+      value = input.slice(tokenStart, index);
+
+      // Decide on the token type and possibly cast the value.
+      if (isKeyword(value)) {
+        type = LuaTokenType.Keyword;
+      } else if ('true' === value || 'false' === value) {
+        type = LuaTokenType.BooleanLiteral;
+      } else if ('nil' === value) {
+        type = LuaTokenType.NilLiteral;
+      } else {
+        type = LuaTokenType.Identifier;
+      }
+      return new LuaToken(type, value, line, lineStart, tokenStart, index);
+    }
+    function readHexLiteral() {
+      var fraction = 0 // defaults to 0 as it gets summed
+        , binaryExponent = 1 // defaults to 1 as it gets multiplied
+        , binarySign = 1 // positive
+        , digit, fractionStart, exponentStart, digitStart;
+  
+      digitStart = index += 2; // Skip 0x part
+  
+      // A minimum of one hex digit is required.
+      if (!isHexDigit(input.charCodeAt(index))) {
+        raiseError(undefined, errorStrings.malformedNumber, input.slice(tokenStart, index));
+      }
+      while (isHexDigit(input.charCodeAt(index))) {
+        index++;
+      }
+      return input.slice(tokenStart, index);
+    }
+
+    function readDecLiteral() {
+      while (isDecDigit(input.charCodeAt(index))) {
+        index++;
+      }
+      // Fraction part is optional
+      if ('.' === input.charAt(index)) {
+        index++;
+        // Fraction part defaults to 0
+        while (isDecDigit(input.charCodeAt(index))) {
+          index++;
+        }
+      }
+      // Exponent part is optional.
+      if (isCharOneOf(input.charAt(index), 'e', 'E')) {
+        index++;
+        // Sign part is optional.
+        if (isCharOneOf(input.charAt(index), '+', '-')) {
+          index++;
+        }
+        // An exponent is required to contain at least one decimal digit.
+        if (!isDecDigit(input.charCodeAt(index))) {
+          raiseError(undefined, errorStrings.malformedNumber, input.slice(tokenStart, index));
+        }
+  
+        while (isDecDigit(input.charCodeAt(index))) {
+          index++;
+        }
+      }
+      return input.slice(tokenStart, index);
+    }
+
+    function scanNumericLiteral() {
+      let character = input.charAt(index);
+      let next = input.charAt(index + 1);
+
+      var value = ('0' === character && (next && (next === 'x' || next === 'X'))) ? readHexLiteral() : readDecLiteral();
+      return new LuaToken(LuaTokenType.NumericLiteral, value, line, lineStart, tokenStart, index);
+    }
+    function scanVarargLiteral() {
+      return new LuaToken(LuaTokenType.EOF, '<eof>', line, lineStart, index, index);
+    }
+    function scanPunctuator(value: string) {
+      index += value.length;
+      return new LuaToken(LuaTokenType.EOF, '<eof>', line, lineStart, tokenStart, index);
+    }
+
+
+    // Find the string literal by matching the delimiter marks used.
+    function scanStringLiteral() {
+      var delimiter = input.charCodeAt(index++)
+        , stringStart = index
+        , string = ''
+        , charCode;
+
+      while (index < length) {
+        charCode = input.charCodeAt(index++);
+        if (delimiter === charCode) {
+          break;
+        }
+        if (92 === charCode) { // \
+          string += input.slice(stringStart, index - 1) + readEscapeSequence();
+          stringStart = index;
+        }
+        // EOF or `\n` terminates a string literal. If we haven't found the
+        // ending delimiter by now, raise an exception.
+        else if (index >= length || isLineTerminator(charCode)) {
+          string += input.slice(stringStart, index - 1);
+          raiseError(undefined, errorStrings.unfinishedString, string + String.fromCharCode(charCode));
+          break;
+        }
+      }
+      string += input.slice(stringStart, index - 1);
+
+      return new LuaToken(LuaTokenType.StringLiteral, string, line, lineStart, tokenStart, index);
+    }
+
+    // Expect a multiline string literal and return it as a regular string
+    // literal, if it doesn't validate into a valid multiline string, throw an
+    // exception.
+
+    function scanLongStringLiteral() {
+      var string = readLongString();
+      // Fail if it's not a multiline literal.
+      if (!string) {
+        raiseError(undefined, errorStrings.expected, '[', '');
+        return new LuaToken(LuaTokenType.Unexpected, '[[', line, lineStart, tokenStart, index);
+      }
+      return new LuaToken(LuaTokenType.StringLiteral, string, line, lineStart, tokenStart, index);
+    }
+
+
+    var charCode = input.charCodeAt(index)
+      , next = input.charCodeAt(index + 1);
+
+
+
+    // Memorize the range index where the token begins.
+    tokenStart = index;
+    if (isIdentifierStart(charCode)) {
+      return scanIdentifierOrKeyword();
+    }
+
+    switch (charCode) {
+      case 39: case 34: // '"
+        return scanStringLiteral();
+
+      // 0-9
+      case 48: case 49: case 50: case 51: case 52: case 53:
+      case 54: case 55: case 56: case 57:
+        return scanNumericLiteral();
+
+      case 46: // .
+        // If the dot is followed by a digit it's a float.
+        if (isDecDigit(next)) {
+          return scanNumericLiteral();
+        }
+        if (46 === next) {
+          if (46 === input.charCodeAt(index + 2)) {
+            return scanVarargLiteral();
+          }
+          return scanPunctuator('..');
+        }
+        return scanPunctuator('.');
+
+      case 61: // =
+        if (61 === next) {
+          return scanPunctuator('==');
+        }
+        return scanPunctuator('=');
+
+      case 62: // >
+        if (61 === next) {
+          return scanPunctuator('>=');
+        }
+        // not Lua5.1
+        // if (62 === next) {
+        //   return scanPunctuator('>>');
+        // }
+        return scanPunctuator('>');
+
+      case 60: // <
+        // not Lua5.1
+        // if (60 === next) {
+        //   return scanPunctuator('<<');
+        // }
+        if (61 === next) {
+          return scanPunctuator('<=');
+        }
+        return scanPunctuator('<');
+
+      case 126: // ~
+        if (61 === next) {
+          return scanPunctuator('~=');
+        }
+        return scanPunctuator('~');
+
+      case 58: // :
+        // not Lua5.1
+        // if (58 === next) {
+        //   return scanPunctuator('::');
+        // }
+        return scanPunctuator(':');
+
+      case 91: // [
+        // Check for a multiline string, they begin with [= or [[
+        if (91 === next || 61 === next) {
+          return scanLongStringLiteral();
+        }
+        return scanPunctuator('[');
+
+      case 47: // /
+        // not Lua5.1.
+        // Check for integer division op (//)
+        // if (47 === next) {
+        //   return scanPunctuator('//');
+        // }
+        return scanPunctuator('/');
+
+      // * ^ % , { } ] ( ) ; # - +
+      case 42: case 94: case 37: case 44: case 123: case 125:
+      case 93: case 40: case 41: case 59:  case 35: case 45: case 43:
+      // not Lua5.1. & |
+      // case 38: case 124:
+        return scanPunctuator(input.charAt(index));
+    }
+    let value = input.charAt(index++);
+    return new LuaToken(LuaTokenType.Unexpected, value, line, lineStart, tokenStart, index);
+  }
+
+  function reset() {
+    index = 0;
+    line = 1;
+    lineStart = 0;
+    length = input.length;
+  }
+  return { getNextToken: getNextToken, reset: reset, raiseError: raiseError, errors: errors};
+}
+
+function isIdentifierStart(charCode: number): boolean {
+  return (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || 95 === charCode;
+}
+
+function isIdentifierPart(charCode: number): boolean {
+  return (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || 95 === charCode || (charCode >= 48 && charCode <= 57);
+}
+
+function isKeyword(id: string) {
+  switch (id.length) {
+    case 2:
+      return 'do' === id || 'if' === id || 'in' === id || 'or' === id;
+    case 3:
+      return 'and' === id || 'end' === id || 'for' === id || 'not' === id;
+    case 4:
+      return 'else' === id || 'goto' === id || 'then' === id;
+    case 5:
+      return 'break' === id || 'local' === id || 'until' === id || 'while' === id;
+    case 6:
+      return 'elseif' === id || 'repeat' === id || 'return' === id;
+    case 8:
+      return 'function' === id;
+  }
+  return false;
+}
+
+function isWhiteSpace(charCode: number) {
+  return 9 === charCode || 32 === charCode || 0xB === charCode || 0xC === charCode;
+}
+
+function isLineTerminator(charCode: number) {
+  return 10 === charCode || 13 === charCode;
+}
+
+function isDecDigit(charCode: number) {
+  return charCode >= 48 && charCode <= 57;
+}
+
+function isHexDigit(charCode: number) {
+  return (charCode >= 48 && charCode <= 57) || (charCode >= 97 && charCode <= 102) || (charCode >= 65 && charCode <= 70);
+}
+
+// A sprintf implementation using %index (beginning at 1) to input
+// arguments in the format string.
+//
+// Example:
+//
+//     // Unexpected function in token
+//     sprintf('Unexpected %2 in %1.', 'token', 'function');
+function sprintf(format: string, ...args: any[]) {
+  let result = format.replace(/%(\d)/g, function (match, index) {
+    return '' + args[index - 1] || '';
+  });
+  return result;
+}
+
+function isCharOneOf(char: string, oneOf0: string, oneOf1: string) {
+  return char === oneOf0 || char === oneOf1;
+}
