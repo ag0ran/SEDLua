aa55a640f8087b9572a020cc96b350284c1bb4fc
unknown <agoran@gmail.com>
2019-11-17 20:30:49 +0100

    Extensive luaParser tests on entire Content dir and matched against the output from the original lua compiler.

--- src/test/suite/luaParser.test.ts
+++ src/test/suite/luaParser.test.ts
@@ -4,6 +4,7 @@ import {readTestFile, expectError} from './luaParserTesting';
 import { Uri } from 'vscode';
 import * as seFilesystem from '../../sefilesystem';
 import fs = require('fs');
+const spawn = require('child_process').spawn;
 
 function testParserErrors() {
   test('Error in local statement', function() {
@@ -39,23 +40,61 @@ function extensiveParserTests() {
   if (process.env.runExtensiveTests !== "1") {
     return;
   }
-  let scriptsRoot = 'd:\\work\\main\\Content';
+  let workspaceRoot = "d:\\work\\main";
+  let scriptsRoot = workspaceRoot + '\\Content';
+  let luacPath = workspaceRoot + "\\Tools\\Windows\\lua\\luac5.1.exe";
+
+  async function compileWithLuac(scriptSource: string): Promise<string|undefined> {
+    let child = spawn(luacPath, [ '-']);
+    return new Promise<string|undefined>((resolve, reject) => {
+      let stdErr = "";
+      child.stderr.on('data', (data: Uint8Array) => {
+        stdErr += `${data}`;
+      });
+      child.on('close', (code: number) => {
+        let result: string|undefined;
+        if (stdErr !== "" && stdErr.indexOf('stdin:') === -1) {
+          result = undefined;
+        } else {
+          result = stdErr;
+        }
+        if (code === 0) {
+          result = "";
+        }
+        resolve(result);
+      });
+      child.stdin.write(scriptSource);
+      child.stdin.end();
+    });
+  }
+
   test('Extensive tests', async function() {
     this.timeout(1e10);
     let numberOfFilesWithErrors = 0;
     let numberOfFilesTested = 0;
-    function parseFileAndCheckForErrors(fileUri: Uri) {
+    async function parseFileAndCheckForErrors(fileUri: Uri) {
       try {
         numberOfFilesTested++;
         let scriptSource = fs.readFileSync(fileUri.fsPath, "utf8");
+        // remove BOM character as lua compiler cannot handle it
+        if (scriptSource.charCodeAt(0) === 65279) {
+          scriptSource = scriptSource.slice(1);
+        }
         let parseResults = parseLuaSource(scriptSource);
-        if (parseResults.errors.length > 0) {
+        let luacResult = await compileWithLuac(scriptSource);
+        if (luacResult !== undefined && (parseResults.errors.length === 0) === (luacResult.length > 0)) {
           numberOfFilesWithErrors++;
-          console.error(`Found ${parseResults.errors.length} errors in ${fileUri.fsPath}:`);
-          for (let error of parseResults.errors) {
-            console.warn(`[${error.line}, ${error.column}]: ${error.message}`);
+          if (parseResults.errors.length > 0) {
+            console.error(`Found ${parseResults.errors.length} errors in ${fileUri.fsPath} not reported by luac:`);
+            for (let error of parseResults.errors) {
+              console.warn(`  [${error.line}, ${error.column}]: ${error.message}`);
+            }
+          } else {
+            console.error(`luaParser found no errors in ${fileUri.fsPath}, but luac reported an error:`);
+              console.warn(`  ${luacResult}`);
           }
         }
+        
       } catch(err) {
         numberOfFilesWithErrors++;
         console.error(`Exception while parsing: ${fileUri.fsPath}\n  ${err.message}`);
